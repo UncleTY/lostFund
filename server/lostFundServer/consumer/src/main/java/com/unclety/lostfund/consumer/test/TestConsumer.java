18c185f4951bd09e4c107c4fbd2622a48fa50337
taoyu <taoyu@yunrong.com>
2020-12-31 09:50:30 +0800

    no message

--- server/lostFundServer/consumer/src/main/java/com/unclety/lostfund/consumer/test/TestConsumer.java
+++ server/lostFundServer/consumer/src/main/java/com/unclety/lostfund/consumer/test/TestConsumer.java
@@ -15,11 +15,7 @@ public class TestConsumer {
 
     @RequestMapping(value = "/hello")
     public String hello() {
-        System.out.println("master");
-        System.out.println("merge");
         String hello = testService.sayHello("world");
-        System.out.println("rebase");
-        System.out.println("rebase2");
         System.out.println(testService.sayHello("BJQ"));
         return hello;
     }
